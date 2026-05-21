1d38cd942b88fe75fd465421a52193cf477e9ea3
Cortez Jesús <cortezfrancisco025@gmail.com>
2026-05-20 21:06:05 -0400

    Maldito jose

--- src/modules/sales/sales.swagger.ts
+++ src/modules/sales/sales.swagger.ts
@@ -7,6 +7,7 @@ import {
   ApiCreatedResponse,
   ApiNoContentResponse,
   ApiBadRequestResponse,
+  ApiBearerAuth,
 } from '@nestjs/swagger';
 
 function createSale() {
@@ -15,6 +16,7 @@ function createSale() {
       summary: 'Crear una venta',
       description: 'Crea una venta en el sistema',
     }),
+    ApiBearerAuth('access-token'),
     ApiCreatedResponse({ description: 'La venta fue creada exitosamente' }),
     ApiNotFoundResponse({ description: 'No existe un cliente, vehiculo, combo o servicios con los id proporcionados' }),
     ApiBadRequestResponse({ description: 'Error en formato de datos' }),
@@ -27,6 +29,7 @@ function updateStatusWashing() {
       summary: 'Actualizar el estado de lavado de una venta',
       description: 'Actualiza el estado de lavado de una venta en el sistema',
     }),
+    ApiBearerAuth('access-token'),
     ApiNoContentResponse({ description: 'La venta fue actualizada exitosamente' }),
     ApiNotFoundResponse({ description: 'No existe una venta con el id proporcionado' }),
     ApiConflictResponse({ description: 'Solo se puede cambiar el estado de lavado de las ventas que esten en Pendiente o En progreso' }),
@@ -39,6 +42,7 @@ function updateStatusPayment() {
       summary: 'Actualizar el estado de lavado de una venta',
       description: 'Actualiza el estado de lavado de una venta en el sistema',
     }),
+    ApiBearerAuth('access-token'),
     ApiNoContentResponse({ description: 'La venta fue actualizada exitosamente' }),
     ApiNotFoundResponse({ description: 'No existe una venta con el id proporcionado' }),
     ApiConflictResponse({ description: 'Solo se puede cambiar el estado de las ventas que esten en Pendiente' }),
@@ -51,6 +55,7 @@ function findAll() {
       summary: 'Obtener todas las ventas',
       description: 'Obtener todas las ventas en el sistema',
     }),
+    ApiBearerAuth('access-token'),
     ApiOkResponse({ description: 'Lista de ventas' }),
   );
 }
